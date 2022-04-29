05f8550e98caf5be696ac38d9674e56dcf1a2db3
Ksenia Grinina <ksenia.grinina@md.p2h.com>
2022-04-29 11:29:47 +0200

    Initial commit

---
+++ build/precache-manifest.148d582a70ab7fda3960dd55ba2bf979.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4026d29eaef8acab6f0db74f5da41d54",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c705f319cd97791fb362",
+    "url": "/static/css/main.b6ff9f56.chunk.css"
+  },
+  {
+    "revision": "bd5ef388df46b95b3c65",
+    "url": "/static/js/2.a93eef23.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/static/js/2.a93eef23.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c705f319cd97791fb362",
+    "url": "/static/js/main.941861a6.chunk.js"
+  },
+  {
+    "revision": "f13d5fb20258b992088c",
+    "url": "/static/js/runtime-main.a6b2f021.js"
+  }
+]);
